5d06d64a72d3406847bf6f35c5c17cb0114c55d4
felipe <enggazolla@gmail.com>
2025-01-24 13:36:56 -0300

    frontend pronto

---
+++ src/app/(pages)/create/page.tsx
@@ -0,0 +1,26 @@
+import { Header } from '@/components/landing/Header'
+import { Button } from '@/components/ui/Button'
+import { TextInput } from '@/components/ui/TextInput'
+import { Rocket } from 'lucide-react'
+
+export default function CreatePage() {
+  return (
+    <div>
+      <Header />
+      <div className="h-screen flex flex-col gap-10 items-center justify-center max-w-xl mx-auto">
+        <div className="flex items-center gap-4">
+          <h1 className="text-4xl font-bold text-white">Escolha seu link</h1>
+          <Rocket className="size-10" />
+        </div>
+        <form action="" className="w-full flex items-center gap-2">
+          <span>projectinbio.com/</span>
+          <TextInput />
+          <Button className="w-[126px]">Criar</Button>
+        </form>
+        <div>
+          <span className="text-accent-pink">Erro de exemplo</span>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,17 @@
+import { Faq } from '@/components/landing/Faq'
+import { Header } from '@/components/landing/Header'
+import { Hero } from '@/components/landing/Hero'
+import { Pricing } from '@/components/landing/Pricing'
+import { VideoExplanation } from '@/components/landing/VideoExplanation'
+
+export default function Home() {
+  return (
+    <div className="max-w-7xl mx-auto h-screen">
+      <Header />
+      <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <Faq />
+    </div>
+  )
+}
