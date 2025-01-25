909edb057242c7256ea3e2340569533ae82123f1
felipe <enggazolla@gmail.com>
2025-01-25 11:07:34 -0300

    mixpanel not setup

---
+++ src/lib/mixpanel.ts
@@ -0,0 +1,9 @@
+// import Mixpanel from 'mixpanel'
+
+// const mixpanelEvent = Mixpanel.init('e16e6bcc9763ae34dc9641b6deb8ef11')
+
+// // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// export function trackServerEvent(eventName: string, properties: any) {
+//   if (process.env.NODE_ENV === 'development') return
+//   mixpanelEvent.track(eventName, properties)
+// }
